b7d156c26bab760f1e99efff54bcef18503b1e70
javafa <nigne2016@gmail.com>
2017-06-08 12:25:08 +0900

    arranged

--- app/src/main/java/com/veryworks/android/handmemo/MainActivity.java
+++ app/src/main/java/com/veryworks/android/handmemo/MainActivity.java
@@ -20,18 +20,21 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     FrameLayout layout;
-    RadioGroup color;
-    SeekBar stroke;
+    RadioGroup color;                  // 색상조절 옵션
+    SeekBar stroke;                    // 두께조절 옵션
 
-    Board board;
+    Board board;                       // 그림판
+
     int opt_brush_color = Color.BLACK; // 브러쉬 색상 기본값
-    float opt_brush_width = 10f;        // 브러쉬 두께 기본값 1
+    float opt_brush_width = 10f;       // 브러쉬 두께 기본값 1
+    /* 브러쉬는 값을 조절할때 마다 그림판에 새로 생성됨 */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        // 그림판이 들어가는 프레임 레이아웃
         layout = (FrameLayout) findViewById(R.id.layout);
         // 색상선택
         color = (RadioGroup) findViewById(R.id.color);
@@ -81,25 +84,27 @@ public class MainActivity extends AppCompatActivity {
 
         // 1. 보드를 새로 생성한다.
         board = new Board(getBaseContext());
-
         // 2. 생성된 보드를 화면에 세팅한다.
         layout.addView(board);
-
         // 3. 기본 브러쉬 세팅
         setBrush();
     }
 
+    /*
+        컬러와 두께는 조절 할 때마다 새로운 브러쉬를 생성하여 그림판에 담는다.
+     */
+    // 컬러 옵션값 조절
     private void setBrushColor(int colorType){
         opt_brush_color = colorType;
         setBrush();
     }
-
+    // 두께 옵션값 조절
     private void setBrushStroke(float width){
         opt_brush_width = width;
         setBrush();
     }
 
-    // 보드에 설정된 브러쉬를 담는다
+    // 현재 설정된 옵션값을 사용하여 브러쉬를 새로 생성하고 그림판에 담는다.
     private void setBrush(){
         Brush brush = new Brush();
         brush.color = opt_brush_color;
@@ -107,21 +112,16 @@ public class MainActivity extends AppCompatActivity {
         board.setBrush(brush);
     }
 
-    class Brush {
-        Path path;
-        int color;
-        float stroke;
-
-        public void addPath(Path path){
-            this.path = path;
-        }
-    }
-
+    /*
+        그림판
+     */
     class Board extends View {
         Paint paint;
         List<Brush> brushes;
         Brush current_brush;
         Path current_path;
+
+        // 브러쉬의 속성값 변경 여부를 판단하기 위한 플래그 > 브러쉬의 속성값이 바뀌면 Path를 다시 생성한다.
         boolean newBrush = true;
 
         public Board(Context context) {
@@ -129,12 +129,7 @@ public class MainActivity extends AppCompatActivity {
             setPaint();
             brushes = new ArrayList<>();
         }
-
-        public void setBrush(Brush brush) {
-            current_brush = brush;
-            newBrush = true;
-        }
-
+        // 처음 한번만 기본 페인트 속성을 설정해둔다.
         private void setPaint(){
             // Paint 의 기본속성만 적용해 두고, color 와 두께는 Brush에서 가져다가 그린다.
             paint = new Paint();
@@ -144,7 +139,12 @@ public class MainActivity extends AppCompatActivity {
             paint.setStrokeCap(Paint.Cap.ROUND);
             paint.setDither(true);
         }
-
+        // 브러쉬를 새로 생성한다.
+        public void setBrush(Brush brush) {
+            current_brush = brush;
+            newBrush = true;
+        }
+        // Path를 새로 생성한다.
         private void createPath(){
             if(newBrush) { // 브러쉬가 변경되었을 때만 Path를 생성해준다.
                 current_path = new Path();
@@ -181,17 +181,29 @@ public class MainActivity extends AppCompatActivity {
                     current_path.lineTo(x,y); // 바로 이전점과 현재점사이에 줄을 그어준다.
                     break;
                 case MotionEvent.ACTION_UP :
+                    // none
                     break;
             }
 
-            // Path 를 그린후 화면을 갱신해서 반영해 준다.
+            // 화면을 갱신해서 위에서 그린 Path를 반영해 준다.
             invalidate();
 
             // 리턴 false 일 경우 touch 이벤트를 연속해서 발생시키지 않는다.
             // 즉, 드래그시 onTouchEvent 가 호출되지 않는다
             return true;
         }
+    }
 
+    /*
+        브러쉬
+     */
+    class Brush {
+        Path path;      // 브러쉬로 그리는 경로를 같이 담아둔다
+        int color;
+        float stroke;
 
+        public void addPath(Path path){
+            this.path = path;
+        }
     }
 }
